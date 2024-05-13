47b195ccc4e7a6dd75e615efb5a6071135204c91
DeepakSilaych <deepaksilaych@gmail.com>
2024-05-13 16:09:40 +0530

    home

--- src/demo.jsx
+++ src/demo.jsx
@@ -2,24 +2,27 @@ import React, { useState } from 'react';
 import Base from './base'; // Assuming Base contains the map component
 import Iii from './iii'; // Assuming Iii contains the widget component
 import Iii2 from './Iil2';
+import Widget from './components/widget/Widget';
+import Widget2 from './components/widget/Widget2';
 
 function Demo() {
   const [selectedOption, setSelectedOption] = useState(null);
+  const [selectedOption1, setSelectedOption1] = useState(null);
   return (
-    <>
-     <h1 style={{ textAlign: 'center', fontWeight: 'bold', fontFamily: 'Arial, sans-serif', fontSize: '2rem', marginTop: '20px' }}>Mumbai Flood Warning System</h1>
-    <div style={{ display: 'flex', width: '100%', height: '100%' }}>
-      
-      <div style={{ width: '100%', height: '87%', overflow: 'hidden' }}>
-        <Base setSelectedOption = {setSelectedOption} />
+    <div className='flex flex-col h-screen w-screen absolute top-0'>
+      <header className='bg-white h-16 flex items-center justify-center shadow-2xl [border-bottom : -1px black]'>
+        <h1 className="text-center font-bold text-2xl">Mumbai Flood Warning System</h1>
+      </header>
+    <div className='flex w-full h-full'>
+      <div className='w-full h-full overflow-hidden'>
+        <Base setSelectedOption={setSelectedOption} selectedOption={selectedOption} setSelectedOption1={setSelectedOption1} selectedOption1={selectedOption1}/>
       </div>
-      <div style={{  width: '40%',height: '50%'}}>
-        <Iii selectedOption={selectedOption}/>
-        <Iii2 selectedOption={selectedOption}/>
-      </div>
-      
+      <div className='w-2/6'>
+        <Widget selectedOption={selectedOption} height={'50%'}/>
+        <Widget2 selectedOption={selectedOption1} height={'50%'}/>
+      </div>  
     </div>
-    </>
+    </div>
   );
 }
 
